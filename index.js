69eb93a90fefe4b997dc8c13d5aefa4ba0977cb1
+ <+>
2023-09-26 12:52:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    const input = document.getElementById("text");
+    input.textContent = "This is really cool!";
+});
